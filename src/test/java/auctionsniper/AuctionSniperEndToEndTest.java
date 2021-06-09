2dbf81c86979ffcb1f2806f53a3a30686889b1cf
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2021-06-09 09:50:31 -0300

    Implementation of end-to-end test to describe the transitions from bidding to winning and back again.

--- src/test/java/auctionsniper/AuctionSniperEndToEndTest.java
+++ src/test/java/auctionsniper/AuctionSniperEndToEndTest.java
@@ -34,6 +34,22 @@ public class AuctionSniperEndToEndTest {
     }
 
     @Test
+    public void sniperWinningUntilOtherSniperMakesAHigherBid() throws Exception {
+        auction.startSellingItem();
+
+        application.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
+
+        auction.reportPrice(1000, 98, ApplicationRunner.SNIPER_XMPP_ID);
+        application.hasShownSniperIsWinning();
+
+        auction.reportPrice(1098, 97, "other bidder");
+        application.hasShownSniperIsBidding();
+
+        auction.hasReceivedBid(1098 + 97, ApplicationRunner.SNIPER_XMPP_ID);
+    }
+
+    @Test
     public void sniperWinsAnAuctionByBiddingHigher() throws Exception {
         auction.startSellingItem();
 
